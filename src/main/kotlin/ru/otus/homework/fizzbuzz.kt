bdd6f4ba08e792c81d7c3a30b3b6c1388e10c9a9
antonkazakov <antonanton08@gmail.com>
2023-04-09 18:31:25 +0400

    ДЗ 1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,22 @@
 package ru.otus.homework
-
+fun main() {
+    println(fizzbuzz(16).contentToString())
+}
 
 fun fizzbuzz(n: Int): Array<String> {
+    var i = 0
+    val array = Array(n) { i++ }
+    val array2 = Array(n) {""}
+    for (x in array) {
+        if ((x == 0) || (x%5 == 0 && x%3 == 0)) {
+            array2[x] = "FizzBuzz"
+        } else if (x%3 == 0) {
+            array2[x] = "Fizz"
+        } else if (x%5 == 0) {
+            array2[x] = "Buzz"
+        } else {
+            array2[x] = x.toString()
+        }
+    }
+    return array2
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,22 @@
 package ru.otus.homework
-
+fun main() {
+    println(sumOfTwo(intArrayOf(0, 1, 2, 3, 4, 5, 6, 7),5).contentToString())
+}
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    val array = Array(2) {0}
+    for (i in numbers.copyOfRange(0, numbers.size - 1)) {
+        for (j in numbers.copyOfRange(i + 1, numbers.size)) {
+            if (numbers[i] + numbers[j] == target) {
+                array [0] = i
+                array [1] = j
+            }
+        }
+    }
+    //return array.toIntArray()
+    return if (array[0] == 0 && array[1] == 0) {
+        throw Exception("IllegalArgumentException")
+    } else {
+        array.toIntArray()
+    }
 }
