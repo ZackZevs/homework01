0cd4ffdcbfc9406a1d2df2ee7c7624f8d33da1f9
ZackZevs <zhekazagor@gmail.com>
2023-04-24 19:09:41 +0700

    ДЗ 1. Исправлено

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -5,18 +5,15 @@ fun main() {
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
     val array = Array(2) {0}
-    for (i in numbers.copyOfRange(0, numbers.size - 1)) {
-        for (j in numbers.copyOfRange(i + 1, numbers.size)) {
+    for (i in numbers.indices) {
+        for (j in i + 1..numbers.lastIndex) {
             if (numbers[i] + numbers[j] == target) {
                 array [0] = i
                 array [1] = j
+                return intArrayOf(i, j)
             }
         }
     }
     //return array.toIntArray()
-    return if (array[0] == 0 && array[1] == 0) {
-        throw Exception("IllegalArgumentException")
-    } else {
-        array.toIntArray()
-    }
+    throw Exception("IllegalArgumentException")
 }
